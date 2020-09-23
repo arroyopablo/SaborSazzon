353252cc9be964621cfe7859ffb7ce7e6332bc9d
arroyopablo <pabloandresarroyoparedes@gmail.com>
2020-09-23 16:59:14 -0500

    correción interfaz

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Mande' });
+    res.render('index', { title: 'Saborsazzon' });
   });
   
   router.get('/about', (req, res) => {
