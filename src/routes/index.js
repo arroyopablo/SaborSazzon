effd0a587110975a512c3b0fd41e7ced7341a2b9
SEBASTIAN SERNA SANCLEMENTE <sebastian.serna@correunivalle.edu.co>
2020-09-25 22:38:50 -0500

    arreglando app, quitando linea de codigo erronea

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { db } = require('./dbConfig');
 const bcrypt = require("bcrypt");
 const session = require('express-session');
 const flash = require('express-flash');
